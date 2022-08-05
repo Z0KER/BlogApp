ab3a40d638a1e98a7021fb4200aee0fdf426c6c8
zoker <flaviogabriel2603@gmail.com>
2022-08-05 19:17:09 -0300

    grupo de rotas

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 // Carregando módulos
-const express = require("express")
-const handlebars = require("express-handlebars")
-const bodyParser = require("body-parser")
-const app = express()
-// const mongoose = require("mongoose")
+    const express = require("express")
+    const expbs = require("express-handlebars")
+    const bodyParser = require("body-parser")
+    const app = express()
+    const admin = require("./routes/admin")
+    // const mongoose = require("mongoose")
 
 // Configurações
-// --Body Parser
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
-// --Handlebars
-app.engine("handlebars", handlebars({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
-
-// --Mongoose
-// Em breve
+    // Body Parser
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+    // Handlebars
+        const hbs = expbs.create({
+            defaultLayout: "main"
+        })
+        app.engine("handlebars", hbs.engine)
+        app.set("view engine", "handlebars")
+    // Mongoose
+    // Em breve
 
 
 
 // Rotas
-
-
+        app.get("/", (req, res) => {
+            res.send("Rota principal")
+        })
+        app.get("/posts", (req, res) => {
+            res.send("Lista Posts")
+        })
+        app.use("/admin", admin)
 
 
 // Outros
-const PORT = 8081
-app.listen(PORT, () => {
-    console.log("Servidor rodando!")
-})
+    const PORT = 8081
+    app.listen(PORT, () => {
+        console.log("Servidor rodando!")
+    })
