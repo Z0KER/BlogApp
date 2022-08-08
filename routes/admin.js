8670b0c4512647395fe80c732d608ef80743b93c
zoker <flaviogabriel2603@gmail.com>
2022-08-08 20:11:41 -0300

    função deletar post adicionada

--- routes/admin.js
+++ routes/admin.js
@@ -57,6 +57,50 @@ router.get('/', (req, res) => {
         }
     })
 
+    router.get('/posts/edit/:id', (req, res) => {
+        Post.findOne({_id: req.params.id}).lean().then((post) => {
+            Category.find().lean().then((categories) => {
+                res.render('admin/editposts', {categories: categories, post: post})
+            }).catch((err) => {
+                req.flash('error_msg', 'An error occurred while trying to list the categories, please try again!')
+                res.redirect('/admin/posts')
+            })
+        }).catch((err) => {
+            req.flash('error_msg', 'An error occurred while trying to load the edit form, please try again!')
+            res.redirect('/admin/posts')
+        })  
+    })
+
+    router.post('/posts/edit', (req, res) => {
+        Post.findOne({_id: req.body.id}).then((post) => {
+            post.title = req.body.title
+            post.slug = req.body.slug
+            post.description = req.body.description
+            post.content = req.body.content
+            post.category = req.body.category
+
+            post.save().then(() => {
+                req.flash('success_msg', 'Post edited successfully!')
+                res.redirect('/admin/posts')
+            }).catch((err) => {
+                req.flash('error_msg', 'An error occurred while trying to change edit form data, please try again!')
+            })
+        }).catch((err) => {
+            req.flash('error_msg', 'An error occurred while trying to save the edit form, please try again!')
+            res.redirect('/admin/posts')
+        })
+    })
+
+    router.post('/posts/delete', (req, res) => {
+        Post.remove({_id: req.body.id}).then(() => {
+            req.flash('success_msg', 'Post deleted successfully!')
+            res.redirect('/admin/posts')
+        }).catch((err) => {
+            req.flash('error_msg', 'An error occurred while trying to delete the post!')
+            res.redirect('/admin/posts')
+        })
+    })
+
 // Categories
     router.get('/categories', (req, res) => {
         Category.find().lean().sort({date: 'desc'}).then((categories) => {
@@ -137,7 +181,7 @@ router.get('/', (req, res) => {
             res.redirect('/admin/categories')
         }).catch((err) => {
             req.flash('error_msg', 'An error occurred while trying to delete the category!')
-            res.redirect('/adimn/categories')
+            res.redirect('/admin/categories')
         })
     })
 
