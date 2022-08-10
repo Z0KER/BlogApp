b0387d7744151cc03d8819d49b994ef9aa8a2a31
zoker <flaviogabriel2603@gmail.com>
2022-08-10 19:05:27 -0300

    cadastrando usuarios no db

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/User')
 const User = mongoose.model('users')
+const bcrypt = require('bcryptjs')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -34,8 +35,41 @@ router.post('/register', (req, res) => {
     if(errors.length > 0) {
         res.render('users/register', {errors: errors})
     } else {
+        User.findOne({email: req.body.email}).then((user) => {
+            if(user) {
+                req.flash('error_msg', 'An account with this e-mail already exists!')
+                res.redirect('/users/register')
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
 
+                bcrypt.genSalt(10, (error, salt) => {
+                    bcrypt.hash(newUser.password, salt, (error, hash) => {
+                        if(error) {
+                            req.flash('error_msg', 'There was an error saving the user!')
+                            res.redirect('/users/register')
+                        } else {
+                            newUser.password = hash
+                            newUser.save().then(() => {
+                                req.flash('success_msg', 'User registered successfully!')
+                                res.redirect('/')
+                            }).catch((err) => {
+                                req.flash('error_msg', 'An error occurred while trying to register the user, please try again!')
+                                res.redirect('/users/register')
+                            })
+                        }
+                    })
+                })
+            }
+        }).catch((err) => {
+            req.flash('error_msg', 'There was an internal error!')
+            res.redirect('/')
+        })
     }
 })
+
 
 module.exports = router
