ca1e682de37529bd20c2f531864d8a0fbb42cb7d
Flávio Viana <75587130+Z0KER@users.noreply.github.com>
2022-08-11 22:06:40 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: 'mongodb+srv://zoker:TafLrfcZzHbQD3LJ@blogapp.08uig8f.mongodb.net/?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://<username>:<password>@blogapp.08uig8f.mongodb.net/?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
